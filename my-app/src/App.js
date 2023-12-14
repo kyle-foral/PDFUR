0b60d80930d65c571eeb7cb92d4439cff327d5f0
Kyle Foral <kyleforal@gmail.com>
2023-12-14 15:39:22 -0800

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import PDF from './PDFPage'
+
+function App() {
+  return (
+    <div className="App">
+      <PDF/>
+    </div>
+  )
+}
+
+export default App;
---
+++ my-app/src/PDFPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Page, Text, Image, Document, StyleSheet } from '@react-pdf/renderer';
+
+// Create styles
+
+const styles = StyleSheet.create({});
+
+// Create Document Component
+const PDF = () => (
+<Document>
+  <Page>
+    <Text>
+
+    </Text>
+    <Image />
+  </Page>
+</Document>
+);
+
+export default PDF;
